cae7e4bd5280e7ea34a6e57fc2c2bff4562cb987
WarrenPieces <christochristoffeldevries@gmail.com>
2023-08-17 16:54:50 -0700

    Shell part one done

--- main.c
+++ main.c
@@ -8,5 +8,16 @@
 
 int main(int ac, char **argv)
 {
+	char *prompt = "(CCS) $";
+	char *line;
+	size_t b = 0;
+
+	(void)ac; (void)argv;
+
+	printf("%s", prompt);
+	getline(&line, &b, stdin);
+	printf("%s\n", line);
+
+	free(line);
 	return(0);
 }
