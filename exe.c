0c6eb0ebea55f1b815725710ac16bd8c2b2ab4dd
CoenraadCrause183 <ravenger183@gmail.com>
2023-08-21 14:17:14 -0700

    update

--- exe.c
+++ exe.c
@@ -3,12 +3,15 @@
 void execmd(char **argv)
 {
 	char *com = NULL;
+	char *true_com = NULL;
 
 	if (argv)
 	{
 		com = argv[0];
+
+		true_com = get_path(com);
 		
-		if (execve(com, argv, NULL) == -1)
+		if (execve(true_com, argv, NULL) == -1)
 		{
 			perror("ERROR...");
 		}
--- get_path.c
+++ get_path.c
@@ -2,13 +2,51 @@
 
 char *get_path(char *com)
 {
-	char *path;
+
+	char *path_m;
 	char *path_c;
-	char com_len;
+	int com_len;
+	char *p_token;
+	int d_len;
+	char *f_path;
+	struct stat buffer;
 
-	path = getenv("PATH");
+	path_m = getenv("PATH");
 
-	path_c = strdup(path);
+	if (path_m)
+	{
+		path_c = strdup(path_m);
+		com_len = strlen(com);
+		p_token = strtok(path_c, ":");
 
+		while (p_token != NULL)
+		{
+			d_len = strlen(p_token);
+			f_path = malloc(com_len + d_len + 2);
 
+			strcpy(f_path, p_token);
+        		strcpy(f_path, "/");
+        		strcpy(f_path, com);
+        		strcpy(f_path, "\0");
+
+			if (stat(f_path, &buffer) == 0)
+			{
+				return (f_path);
+			}
+		}
+	}
+		else
+		{
+			p_token = strtok(NULL, ":");
+		}
+
+	if (stat(com, &buffer) == 0)
+	{
+		return (com);
+	}
+	
+	free(path_c);
+	free(f_path);
+
+	return (NULL);
 }
