cb29c58d3c7019d4ce4202e300203494afdc9c7c
CoenraadCrause183 <ravenger183@gmail.com>
2023-08-21 05:40:43 -0700

    update

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 
 int main(int ac, char **argv)
 {
-	char *main_com = NULL;
-	char *c_com = NULL;
+	char *prompt = "(CCS) $ ";
+	char *line = NULL, *line_c = NULL;
 	size_t b = 0;
 	ssize_t char_read;
 	char *line_c = NULL;
@@ -30,45 +30,46 @@ int main(int ac, char **argv)
                         printf("Ending Shell...\n");
                         return (-1);
                 }
-        }
 
-        return(0);
+		line_c = malloc(sizeof(char) * char_read);
 
-	line_c = malloc(sizeof(char) * char_read);
+		if (line_c == NULL)
+		{
+			perror("Memory allocation error");
+			return (-1);
+		}
 
-	if (line_c == NULL)
-	{
-		perror("Memory allocation error");
-		return (-1);
-	}
-
-	strcpy(line_c, line);
+		strcpy(line_c, line);
 	
-	tkn = strtok(line, dlim);
+		tkn = strtok(line, dlim);
 
-	while (tkn != NULL)
-	{
+		while (tkn != NULL)
+		{
+			tkn_num++;
+			tkn =strtok(NULL, dlim);
+		}
+
 		tkn_num++;
-		tkn =strtok(NULL, dlim);
+
+		argv = malloc(sizeof(char *) * tkn_num);
+
+		tkn = strtok(line_c, dlim);
+
+		for (i1 = 0; tkn != NULL; i1++)
+		{
+			argv[i1] = malloc(sizeof(char) * strlen(tkn));
+			strcpy(argv[i1], tkn);
+
+			tkn = strtok(NULL, dlim);
+		}
+
+		argv[i1] = NULL;
+
+		execmd(argv);
 	}
-
-	tkn_num++;
-
-	argv = malloc(sizeof(char *) * tkn_num);
-
-	tkn = strtok(line_c, dlim);
-
-	for (i1 = 0; tkn != NULL, i1++)
-	{
-		argv[i1] = malloc(sizeof(char) * strlen(tkn));
-		strcpy(argv[i1], tkn);
-
-		tkn = strtok(NULL, dlim);
-	}
-
-	argv[i1] = NULL;
-
-	printf("%s\n", line);
 
 	free(line);
+	free(line_c);
+
+	return (0);
 }
